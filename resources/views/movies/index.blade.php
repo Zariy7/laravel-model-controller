3fac3aab8d41f2b277733258401fd8569cc02fa6
Zariy7 <nazariy.hereha@gmail.com>
2024-02-15 09:12:32 +0100

    movie cards (imageless)

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,20 +5,45 @@
         <div class="row g-5">
             @foreach ($movies_db as $movie)
             <div class="col-3">
-                <div>
-                    {{ $movie['title'] }}
-                </div>
-                <div>
-                    {{ $movie['original_title'] }}
-                </div>
-                <div>
-                    {{ $movie['nationality'] }}
-                </div>
-                <div>
-                    {{ $movie['date'] }}
-                </div>
-                <div>
-                    {{ $movie['vote'] }}
+                <div class="border border-primary rounded bg-info p-1">
+                    <div>
+                        <span class="fw-bold">
+                            Titolo:
+                        </span>
+                        {{ $movie['title'] }}
+                    </div>
+                    <div>
+                        <span class="fw-bold">
+                            Titolo Originale:
+                        </span>
+                        {{ $movie['original_title'] }}
+                    </div>
+                    <div>
+                        @php
+                            $nationalities = explode('/', $movie['nationality']);
+                        @endphp
+                        <span class="fw-bold">
+                            Paese:
+                        </span>
+                        @if(in_array('american', $nationalities))
+                            <span class="fi fi-us"></span>
+                        @endif
+                        @if(in_array('british', $nationalities))
+                            <span class="fi fi-gb"></span>
+                        @endif
+                    </div>
+                    <div>
+                        <span class="fw-bold">
+                            Rilascio:
+                        </span>
+                        {{ date("d-m-Y", strtotime($movie['date'])) }}
+                    </div>
+                    <div>
+                        <span class="fw-bold">
+                            Voto:
+                        </span>
+                        {{ $movie['vote'] }}/10
+                    </div>
                 </div>
             </div>
             @endforeach
